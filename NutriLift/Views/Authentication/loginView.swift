e6e06d45e1b01c7054d8d611c91683e018d7464a
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-02 13:22:51 -0500

    intial login view commit

---
+++ NutriLift/Views/Authentication/loginView.swift
@@ -0,0 +1,18 @@
+//
+//  loginView.swift
+//  NutriLift
+//
+//  Created by Mehraj Hasan on 3/2/25.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginView()
+}
