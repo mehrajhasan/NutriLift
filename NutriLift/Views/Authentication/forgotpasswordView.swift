759c899063b8983feb966dd2ad5509612218e2bb
Mohammad <mohammadhossain13.mh@gmail.com>
2025-03-02 14:18:02 -0500

    Adding sign up and forgot password views

---
+++ NutriLift/Views/Authentication/forgotpasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  forgotpasswordView.swift
+//  NutriLift
+//
+//  Created by Lubna Asha on 3/2/25.
+//
+
+import SwiftUI
+
+struct forgotpasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    forgotpasswordView()
+}
---
+++ NutriLift/Views/Authentication/signupView.swift
@@ -0,0 +1,18 @@
+//
+//  signupView.swift
+//  NutriLift
+//
+//  Created by Lubna Asha on 3/2/25.
+//
+
+import SwiftUI
+
+struct signupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    signupView()
+}
