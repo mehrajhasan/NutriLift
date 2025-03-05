b174951c96361e27f2be2f8e72a11fac1149f322
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-05 13:34:41 -0500

    wrapped funcs in NavigationStack to fix flow to other pages

--- NutriLift/Views/Authentication/loginView.swift
+++ NutriLift/Views/Authentication/loginView.swift
@@ -13,113 +13,115 @@ struct loginView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack{
-            Text("NutriLift")
-                .font(.largeTitle)
-                .bold()
-                .padding(.top,80)
-            
-            Spacer()
-    
-            TextField("Username", text: $username)
-                .foregroundColor(.white)
-                .padding(.horizontal)
-                .frame(height:50)
-                .background(
-                    RoundedRectangle(cornerRadius: 25)
-                    .fill(Color(hue: 0.0, saturation: 0.0, brightness: 0.1686))
-                    
-                )
-                .padding(.horizontal, 50)
-                .padding(.bottom,-10)
-                .overlay(
-                    Group{
-                        if username.isEmpty {
-                            Text("Username")
-                                .foregroundColor(.white)
-                                .padding(.horizontal, -130)
-                                .padding(.bottom,-10)
-                        }
-                    }
-                )
-
+        NavigationStack {
+            VStack{
+                Text("NutriLift")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top,80)
                 
-            SecureField("Password", text: $password)
-                .foregroundColor(.white)
-                .padding(.horizontal)
-                .frame(height:50)
-                .background(
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(Color(hue: 0.0, saturation: 0.0, brightness: 0.1686))
-                    
-                )
-                .padding(.horizontal, 50)
-                .overlay(
-                    Group{
-                        if password.isEmpty {
-                            Text("Password")
-                                .foregroundColor(.white)
-                                .padding(.horizontal, -130)
-                                .padding(.bottom,-3)
+                Spacer()
+                
+                TextField("Username", text: $username)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .frame(height:50)
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(hue: 0.0, saturation: 0.0, brightness: 0.1686))
+                        
+                    )
+                    .padding(.horizontal, 50)
+                    .padding(.bottom,-10)
+                    .overlay(
+                        Group{
+                            if username.isEmpty {
+                                Text("Username")
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal, -130)
+                                    .padding(.bottom,-10)
+                            }
                         }
-                    }
-                )
-                .padding(.top,30)
-                .padding(.bottom,-40)
-                .padding(.vertical,-10)
-            
-
-        }
-        HStack{
-            Spacer()
-            NavigationLink{
-                forgotpasswordView()
-            } label: {
-                Text("Forgot password?").underline()
+                    )
+                
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .frame(height:50)
+                    .background(
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color(hue: 0.0, saturation: 0.0, brightness: 0.1686))
+                        
+                    )
+                    .padding(.horizontal, 50)
+                    .overlay(
+                        Group{
+                            if password.isEmpty {
+                                Text("Password")
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal, -130)
+                                    .padding(.bottom,-3)
+                            }
+                        }
+                    )
+                    .padding(.top,30)
+                    .padding(.bottom,-40)
+                    .padding(.vertical,-10)
+                
+                
             }
+            HStack{
+                Spacer()
+                NavigationLink{
+                    forgotpasswordView()
+                } label: {
+                    Text("Forgot password?").underline()
+                }
                 .padding(.horizontal,50)
                 .padding(.top,50)
                 .foregroundColor(Color(hue: 0.6667, saturation: 1.0, brightness: 1.0))
                 
-        }
-        VStack{
-            Spacer()
-            Button(action: {
-                //check user in database
-                //just checking if flows correctly
-                print("Username: \(username)")
-                print("Password: \(password)")
-            }){
-                Text("Sign in")
-                    .foregroundColor(.white)
-                    .bold()
-                    .font(.title2)
-                    .frame(height:40)
-                    .padding(.horizontal, 110)
-                    .background(
-                        RoundedRectangle(cornerRadius: 25)
-                            .fill(Color(hue: 0.55033,saturation: 0.9608,brightness: 1))
-                        
-                    )
-                    .padding(.vertical,75)
             }
-
-            Text("New to NutriLift?")
-                .padding(.top,-65)
-                .foregroundColor(Color(hue: 0.6667, saturation: 1.0, brightness: 1.0))
-            
-            NavigationLink{
-                signupView()
-            } label: {
-                Text("Sign up!")
-                    .underline()
-            }
+            VStack{
+                Spacer()
+                Button(action: {
+                    //check user in database
+                    //just checking if flows correctly
+                    print("Username: \(username)")
+                    print("Password: \(password)")
+                }){
+                    Text("Sign in")
+                        .foregroundColor(.white)
+                        .bold()
+                        .font(.title2)
+                        .frame(height:40)
+                        .padding(.horizontal, 110)
+                        .background(
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(Color(hue: 0.55033,saturation: 0.9608,brightness: 1))
+                            
+                        )
+                        .padding(.vertical,75)
+                }
+                
+                Text("New to NutriLift?")
+                    .padding(.top,-65)
+                    .foregroundColor(Color(hue: 0.6667, saturation: 1.0, brightness: 1.0))
+                
+                NavigationLink{
+                    signupView()
+                } label: {
+                    Text("Sign up!")
+                        .underline()
+                }
                 .padding(.top,-55)
                 .foregroundColor(Color(hue: 0.6667, saturation: 1.0, brightness: 1.0))
+            }
+            
+            
+            
         }
-        
-        
-        
     }
 }
 
