0ad1ce363718c6de49792a65d9bd0196b6fca9e2
ItWrecks <jairo.iqbalgil58@myhunter.cuny.edu>
2025-03-05 16:43:21 -0500

    fixed backbutton fixeed colors

--- NutriLift/Views/Authentication/forgotpasswordView.swift
+++ NutriLift/Views/Authentication/forgotpasswordView.swift
@@ -1,34 +1,12 @@
-//
-//  forgotpasswordView.swift
-//  NutriLift
-//
-//  Created by Jairo Iqbal Gil on 3/2/25.
-//
-
 import SwiftUI
 
 struct forgotpasswordView: View {
     @State private var email: String = ""
     @State private var isEmailSent = false
-    @Environment(\.dismiss) private var dismiss // Allows dismissing the screen
 
     var body: some View {
         NavigationStack {
             VStack(spacing: 20) {
-                // Back button at the top left
-                HStack {
-                    Button(action: {
-                        dismiss() // Go back to the previous screen
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .font(.title)
-                            .foregroundColor(.blue)
-                    }
-                    Spacer()
-                }
-                .padding(.leading, 20)
-                .padding(.top, 10)
-
                 Text("NutriLift")
                     .font(.largeTitle)
                     .fontWeight(.bold)
@@ -86,47 +64,6 @@ struct forgotpasswordView: View {
 
     func sendResetLink() {
         isEmailSent = true
-    }
-}
-
-
-// Custom text field
-struct CustomTextField: View {
-    var placeholder: String
-    @Binding var text: String
-
-    var body: some View {
-        HStack {
-            Image(systemName: "envelope")
-                .foregroundColor(.gray)
-                .padding(.leading, 10)
-
-            TextField(placeholder, text: $text)
-                .foregroundColor(.white)
-                .autocapitalization(.none)
-                .keyboardType(.emailAddress)
-        }
-        .padding()
-        .background(RoundedRectangle(cornerRadius: 10).stroke(Color(hex: "0AB5FF"), lineWidth: 1))
-        .background(Color(hex: "2B2B2B"))
-        .cornerRadius(10)
-    }
-}
-
-// did this so we can use the hex codes directly from figma
-extension Color {
-    init(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexSanitized).scanHexInt64(&rgbValue)
-
-        let red = Double((rgbValue >> 16) & 0xFF) / 255.0
-        let green = Double((rgbValue >> 8) & 0xFF) / 255.0
-        let blue = Double(rgbValue & 0xFF) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
     }
 }
 
