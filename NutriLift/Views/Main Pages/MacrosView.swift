ceeb86935c7fbc67be34432aa0b426962a3a1768
Mohammad <mohammadhossain13.mh@gmail.com>
2025-03-09 15:41:03 -0400

    Home page - Macros page first draft

---
+++ NutriLift/Views/Main Pages/MacrosView.swift
@@ -0,0 +1,18 @@
+//
+//  MacrosView.swift
+//  NutriLift
+//
+//  Created by Lubna Asha on 3/9/25.
+//
+
+import SwiftUI
+
+struct MacrosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MacrosView()
+}
