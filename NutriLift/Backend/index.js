8c0de2fe43292ef9bf508247b1947fda12331c4c
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-03-09 16:59:20 -0400

    login backend logic implemeneted

--- NutriLift/Backend/index.js
+++ NutriLift/Backend/index.js
@@ -22,6 +22,35 @@ app.get('/users', async (req,res) => {
     }
 });
 
+//login function
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const check = await db.query("SELECT * FROM users WHERE username = $1", [
+            username,
+        ]);
+
+        if(check.rows.length>0){
+            const user = check.rows[0];
+            const pass = check.password;
+
+            if(password == pass){
+                res.status(200).json({ message: "Login Successful" });
+            }
+            else{
+                res.status(400).json({ message: "Incorrect Password" });
+            }
+        }
+        else{
+            res.status(404).json({ message: "User not found" });
+        }
+
+    }
+    catch(err){
+        res.send(err.message);
+    }
+})
 
 app.listen(3000, () => {
     console.log(`Server running on http://localhost:3000/`);
