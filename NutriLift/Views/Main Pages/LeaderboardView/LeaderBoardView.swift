3a9d9e45663834180b21cdaa061b9f2d2d03f354
Mehraj Hasan <mehrajhasancs@gmail.com>
2025-04-28 03:07:16 -0400

    ui mockup of leaderboard

--- NutriLift/Views/Main Pages/LeaderboardView/LeaderBoardView.swift
+++ NutriLift/Views/Main Pages/LeaderboardView/LeaderBoardView.swift
@@ -2,35 +2,55 @@
 //  LeaderBoardView.swift
 //  NutriLift
 //
-//  Created by Jairo Iqbal Gil on 3/9/25.
+//  Created by Mehraj Hasan on 3/9/25.
 //
 
 import SwiftUI
 
 struct LeaderBoardView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Spacer()
-            
-            Text("Leaderboard Coming Soon")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(.black)
-            
-            Text("We're working on the leaderboard feature. Stay tuned for updates!")
-                .font(.title3)
-                .foregroundColor(.gray.opacity(0.7))
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 30)
-            
-            Spacer()
+        NavigationStack{
+            VStack(spacing: 20) {
+                HStack{
+                    Spacer()
+                    Text("18")
+                    Spacer()
+                    Circle()
+                        .fill(Color.black)
+                        .frame(width: 50, height: 50)
+                        .overlay(
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .padding(25)
+                                .foregroundColor(Color(red: 0.4, green: 0.3, blue: 0.6))
+                        )
+                    Spacer()
+                    Text("John Doe").bold()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Text("192")
+                    Spacer()
+                    Image(systemName:"arrow.up.right.circle")
+                    Spacer()
+                }
+                .padding()
+                .background(Color(red: 0.4, green: 0.698, blue: 0.941))                .cornerRadius(15)
+                .foregroundColor(.white)
+                
+                Spacer()
+            }
+            .navigationTitle("Leaderboard")
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
-        .edgesIgnoringSafeArea(.all)
     }
+    
+
 }
 
 #Preview {
-    TaskBarView()
+    LeaderBoardView()
 }
