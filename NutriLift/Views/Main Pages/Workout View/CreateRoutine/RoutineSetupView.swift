53ea9819a84efa554ed4d2e6608c27ae662db6d9
ItWrecks <jairo.iqbalgil58@myhunter.cuny.edu>
2025-05-06 01:14:35 -0400

    building routine setup screen with routine name input and exercise preview

--- NutriLift/Views/Main Pages/Workout View/CreateRoutine/RoutineSetupView.swift
+++ NutriLift/Views/Main Pages/Workout View/CreateRoutine/RoutineSetupView.swift
@@ -7,28 +7,33 @@
 
 import SwiftUI
 
+
+// screen for setting up a new workout routine
 struct RoutineSetupView: View {
     @State private var routineName: String = ""
     @State private var selectedExercises: [ExerciseEntry] = []
     @State private var showExercisePicker = false
-
+    
     var body: some View {
         VStack {
+            //title
             Text("Create a Routine")
                 .font(.title)
                 .fontWeight(.bold)
                 .padding(.top)
-
+            
+            //input for the routine name
             TextField("Enter Routine Name", text: $routineName)
                 .padding()
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(10)
                 .padding(.horizontal)
-
+            
+            //Label aboce the list
             Text("Exercises in Routine:")
                 .font(.headline)
                 .padding(.top)
-
+            
             ScrollView {
                 VStack(spacing: 16) {
                     ForEach($selectedExercises) { $entry in
@@ -38,10 +43,10 @@ struct RoutineSetupView: View {
                 .padding(.horizontal)
             }
             .frame(height: 300)
-
-
+            
+            
             Spacer()
-
+            
             Button(action: {
                 showExercisePicker = true
             }) {
@@ -54,7 +59,8 @@ struct RoutineSetupView: View {
                     .cornerRadius(12)
             }
             .padding(.horizontal)
-
+            
+            // navigation link to save the routine, only enabled if name and exercises are filled
             NavigationLink(destination: SaveRoutineView(routineName: routineName, exercises: selectedExercises)) {
                 Text("Save Routine")
                     .bold()
@@ -66,9 +72,10 @@ struct RoutineSetupView: View {
             }
             .padding(.horizontal)
             .disabled(routineName.isEmpty || selectedExercises.isEmpty) // Prevent saving if no data
-
+            
         }
         .padding()
+        // triggers the exercise selection screen when user taps "add exercises"
         .navigationDestination(isPresented: $showExercisePicker) {
             ExerciseSelectionView(selectedExercises: $selectedExercises)
         }
