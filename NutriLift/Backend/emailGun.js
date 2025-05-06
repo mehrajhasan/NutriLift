6e3995b855d0a3c9e8858b1e3465ca7db9260ea2
ItWrecks <jairo.iqbalgil58@myhunter.cuny.edu>
2025-05-06 01:05:22 -0400

    added exercise selection view with search and backend fetch

---
+++ NutriLift/Backend/emailGun.js
@@ -0,0 +1,30 @@
+//
+//  emailGun.js
+//  NutriLift
+//
+//  Created by Jairo Iqbal Gil on 4/4/25.
+
+//copied what is below from SendGrid Intergration Guide
+
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+javascript
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'oriaj1630@gmail.com', // Change to your recipient
+  from: 'oriaj1630@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
